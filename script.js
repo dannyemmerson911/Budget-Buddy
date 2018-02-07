52c0324f829691cf369ba40fc72a8f5c46619e7c
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-02-07 15:49:35 -0500

    comments with JS plan of action

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ var userWeeklyBudget;
 	});
 
 });
+
+//1. Fade out welcome page on click
+//2. Find budget input value
+//3. Append budget value to "total budget"
+//4. Make sure all inputs have values on submit
+//    - Alert user that inputs must be filled
+//5. Find input values for "amount spent" and "item" on click
+//6. Subtract "amount spent" from "total budget"
+//7. Add "amount spent" to "total spent"
+//8. Add amount spent to appropriate category, based on what category was selected in dropdown
+//9. Append "item" and "individual expense" to hidden div based on category
+//10. Once "total budget" is less than $100, fade in first pop up alert
+//11. Once "total budget" reaches $0, fade in second popup alert
+//12. Hide main form, ask user if they'd like to budget for another week
