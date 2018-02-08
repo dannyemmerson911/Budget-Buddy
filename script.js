ce02866878a04b5e9d004133b04420356ab7eb90
Mohammed Fahad <mfahad24@users.noreply.github.com>
2018-02-07 21:06:40 -0500

    more changes

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
 $(document).ready(function() {
 
-var userWeeklyBudget;
+  var userWeeklyBudget;
 
-	$("#wrapperdiv").hide();
+  $("#wrapperdiv").hide();
 
-	$("#lessThanHundred").hide();
+  $("#lessThanHundred").hide();
 
-	$("#noMoney").hide();
+  $("#noMoney").hide();
 
-	$("#budgetSubmitButton").click(function(event){
-		event.preventDefault();
-		userWeeklyBudget = $("#userBudgetInput").val();
-		console.log(userWeeklyBudget);
-		$("#userBudgetInput").val("");
-		$("#budget").append(userWeeklyBudget);
-		$("#budgetInputDiv").hide();
-		$("#wrapperdiv").show();
-	});
+  $("#budgetSubmitButton").click(function(event) {
+    event.preventDefault();
+    userWeeklyBudget = $("#userBudgetInput").val();
+    console.log(userWeeklyBudget);
+    $("#userBudgetInput").val("");
+    $("#budget").append(userWeeklyBudget);
+    $("#budgetInputDiv").hide();
+    $("#wrapperdiv").show();
+  });
 
-	$("#formsubmit").click(function(event){
-		event.preventDefault(); 
-		var item = $("#item").val();
-		var price = $("#price").val();
+  $("#formsubmit").click(function(event) {
+    event.preventDefault();
+    var item = $("#item").val();
+    var price = $("#price").val();
 
-		var itemIsEmpty = (item === "");
-		var priceIsEmpty = (price === "");
+    var itemIsEmpty = (item === "");
+    var priceIsEmpty = (price === "");
 
-		if (itemIsEmpty || priceIsEmpty) {
-			alert("Please fill out the form");
-		}
-		var remaining = userWeeklyBudget-price;
-		$("#budget").text("$"+remaining); 
+    if (itemIsEmpty || priceIsEmpty) {
+      alert("Please fill out the form");
+    }
+    var remaining = userWeeklyBudget - price;
+    $("#budget").text("$" + remaining);
 
 
+  });
 
-	});
 
-		
 
 
 });
@@ -46,7 +45,7 @@ var userWeeklyBudget;
 //3. Append budget value to "total budget" ================ DONE
 //4. Make sure all inputs have values on submit ================ DONE
 //    - Alert user that inputs must be filled ================ DONE
-//5. Find input values for "amount spent" and "item" on click 
+//5. Find input values for "amount spent" and "item" on click
 //6. Subtract "amount spent" from "total budget"
 //7. Add "amount spent" to "total spent"
 //8. Add amount spent to appropriate category, based on what category was selected in dropdown
