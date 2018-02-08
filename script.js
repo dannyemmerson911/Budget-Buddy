643b84f3b85a1f8060f66250c75a26e2b36bc834
Mohammed Fahad <mfahad24@users.noreply.github.com>
2018-02-08 11:20:09 -0500

    changes

--- script.js
+++ script.js
@@ -1,46 +1,41 @@
 $(document).ready(function() {
-
-	var userWeeklyBudget;
-	var price;
-	var initialSpent = 0;
-
-
-
-  //on page load, hides popup warning divs and the div containing the main expense tracker
-  $("#wrapperdiv").hide();
-  $("#lessThanHundred").hide();
-  $("#noMoney").hide();
-
-  //on submit button click:
-  // - assigns userWeeklyBudget input to the variable "userWeeklyBudget"
-  // - clears the input field
-  // - appends the user's weekly bugdet input value to the expense tracker div
-  // - hides the main div
-  // - shows the expense tracker div
+    var userWeeklyBudget;
+    var price;
+    var initialSpent = 0;
+    var entertainmentTotal = 0;
+    var foodTotal = 0;
+    var clothingTotal = 0;
+    var billsTotal = 0;
+//on page load, hides popup warning divs and the div containing the main expense tracker
+    $("#wrapperdiv").hide();
+    $("#lessThanHundred").hide();
+    $("#noMoney").hide();
+//on submit button click:
+// - assigns userWeeklyBudget input to the variable "userWeeklyBudget"
+// - clears the input field
+// - appends the user's weekly bugdet input value to the expense tracker div
+// - hides the main div
+// - shows the expense tracker div
   $("#budgetSubmitButton").click(function(event) {
     event.preventDefault();
     userWeeklyBudget = $("#userBudgetInput").val();
-
     $("#userBudgetInput").val("");
     $("#budget").append("$" + userWeeklyBudget);
     $("#budgetInputDiv").hide();
     $("#wrapperdiv").show();
   });
-
-  //on form submit button click:
-  // - grabs user input values for item and price of item, and assigns them to variables "item" and "price"
-  // - if the price or item input values are empty, alert the user that inputs must be filled in
-  // - subtracts the price of the item from the userWeeklyBudget total
-  // - displays the updated userWeeklyBudget total in the h2 element with the id of "budget"
+//on form submit button click:
+// - grabs user input values for item and price of item, and assigns them to variables "item" and "price"
+// - if the price or item input values are empty, alert the user that inputs must be filled in
+// - subtracts the price of the item from the userWeeklyBudget total
+// - displays the updated userWeeklyBudget total in the h2 element with the id of "budget"
   $("#formsubmit").click(function(event) {
     event.preventDefault();
-    var item = $("#item").val();
-    price = Number($("#price").val());
-    var moneySpentEl = $("#moneyspent");
-
+        var item = $("#item").val();
+        price = Number($("#price").val());
+        var moneySpentEl = $("#moneyspent");
     var itemIsEmpty = (item === "");
     var priceIsEmpty = (price === "");
-
     if (itemIsEmpty || priceIsEmpty) {
       alert("Please fill out the form");
     }
@@ -49,44 +44,27 @@ $(document).ready(function() {
     initialSpent = price += Number(initialSpent);
     // console.log(initialSpent);
     moneySpentEl.text("$" + initialSpent);
-
 // Below here is what I added
 //
-		// $("#moneyspent").text("$" + price);
-
-		// userWeeklyBudget = remaining;
-		// $("#item").val("");
-		// $("#price").val("");
+        // $("#moneyspent").text("$" + price);
+        // userWeeklyBudget = remaining;
+        // $("#item").val("");
+        // $("#price").val("");
   // });
+    userWeeklyBudget = remaining;
+    moneySpentEl = initialSpent;
+    var selected = $('categoryChoice :selected').text();
+  categoryChoice = $("#categoryChoice").selected;
 
-
-
-	userWeeklyBudget = remaining;
-	moneySpentEl = initialSpent;
-	var selected = $('categoryChoice :selected').text();
-
-
-   	var entertainmentUpdate = Number(entertainmentTotal) + price;
+    var entertainmentUpdate = Number(entertainmentTotal) + price;
     console.log(entertainmentTotal);
-
     $("#entertainmentEl").text("$" + entertainmentUpdate);
 
-
   $("#item").val("");
-	$("#price").val("");
-
-
-
-
+    $("#price").val("");
   });
 
-
-
-
-
-
-});
-
+}); 
 //1. Fade out welcome page on click ================ DONE
 //2. Find budget input value ================ DONE
 //3. Append budget value to "total budget" ================ DONE
