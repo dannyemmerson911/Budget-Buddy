f4819a3e46d02c2ba681a523ff6fcad820b935a7
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-02-09 11:36:05 -0500

    deleted 'to-do' comments

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ $(document).ready(function() {
     $("#budgetInputDiv").fadeOut(100);
     $("#wrapperdiv").fadeIn(1000);
   });
+
   //on form submit button click:
   // - grabs user input values for item and price of item, and assigns them to variables "item" and "price"
   // - if the price or item input values are empty, alert the user that inputs must be filled in
@@ -111,16 +112,3 @@ $(document).ready(function() {
 
 });
 
-//1. Fade out welcome page on click ================ DONE
-//2. Find budget input value ================ DONE
-//3. Append budget value to "total budget" ================ DONE
-//4. Make sure all inputs have values on submit ================ DONE
-//    - Alert user that inputs must be filled ================ DONE
-//5. Find input values for "amount spent" and "item" on click ===========DONE
-//6. Subtract "amount spent" from "total budget" ==============DONE
-//7. Add "amount spent" to "total spent" ===========DONE
-//8. Add amount spent to appropriate category, based on what category was selected in dropdown
-//9. Append "item" and "individual expense" to hidden div based on category
-//10. Once "total budget" is less than $100, fade in first pop up alert
-//11. Once "total budget" reaches $0, fade in second popup alert
-//12. Hide main form, ask user if they'd like to budget for another week++++++++++++++
